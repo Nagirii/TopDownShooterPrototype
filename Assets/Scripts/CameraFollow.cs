cfc71588f4c5193025cf28b72ff20e7d767513c7
Zeca <zecaspindola323@gmail.com>
2019-06-13 12:09:39 -0300

    FINALMENTE PUSHANDO ESSA PORRA

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float speed;
+
+    private void Start()
+    {
+        transform.position = playerTransform.position;
+    }
+
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = Vector2.Lerp(transform.position, playerTransform.position, speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/DefaultProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultProjectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public ParticleSystem deathParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+    void DestroyProjectile()
+    {
+        Instantiate(deathParticle, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
