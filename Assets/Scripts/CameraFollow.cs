18f2da7d94f8e5ce08d5e1104afe1d1f97911456
Aluno <aluno@udicentro.local>
2019-08-01 11:36:46 -0300

    Camera follow com restrição

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,11 @@ public class CameraFollow : MonoBehaviour
     public Transform playerTransform;
     public float speed;
 
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
     private void Start()
     {
         transform.position = playerTransform.position;
@@ -16,7 +21,10 @@ public class CameraFollow : MonoBehaviour
     {
         if (playerTransform != null)
         {
-            transform.position = Vector2.Lerp(transform.position, playerTransform.position, speed);
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX);
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+
+            transform.position = Vector2.Lerp(transform.position, new Vector2(clampedX,clampedY), speed);
         }
     }
 }
