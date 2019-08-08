0dfad60cb815574f4064643bb5231fbbdf4fb873
Aluno <aluno@udicentro.local>
2019-08-08 12:19:58 -0300

    Porta funcionando sem crashar dessa vez

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+
+    public string sceneName;
+    public float xPosition;
+    public float yPosition;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Input.GetKeyDown("e")) { 
+            collision.transform.position = new Vector3(xPosition, yPosition, 0);
+            LoadScene(sceneName);
+            }
+        }
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/Scripts/ChutaPorta.cs
+++ Assets/Scripts/ChutaPorta.cs
@@ -5,34 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class ChutaPorta : MonoBehaviour
 {
-  bool playerContact;
-  public Transform target;
+    bool playerContact;
+    public Transform target;
+    public float xPosition;
+    public float yPosition;
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetKeyDown("e") && playerContact == true){
-                 DontDestroyOnLoad(target.gameObject);
-         SceneManager.LoadScene("k");
-            }
-            
-             
-    
+        if (Input.GetKeyDown("e") && playerContact == true)
+        {
+            DontDestroyOnLoad(target.gameObject);
+            target.position = new Vector3(xPosition, yPosition, 0);
+            SceneManager.LoadScene("k");
+        }
+
+        
+
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag == "Player"){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
             playerContact = true;
-            }
-            else
-            {
-                playerContact = false;
-            }
-            }  
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerContact = false;
+        }
+    }
+
+
+    public void LoadScene(string sceneName)
+{
+    SceneManager.LoadScene(sceneName);
+}
+}
+ 
   
-    }
-    public void LoadScene(string sceneName){
-         SceneManager.LoadScene(sceneName);
-    }
-}
-}
+
+
+
 
