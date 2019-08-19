0ff01c039a8664a4e7c56704bfa0cb443b54606e
SarahHessel <sarah.phessel@gmail.com>
2019-08-19 11:05:20 -0300

    SPAWNER MT MASSA TO FELIZ

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject monster;
+    public Transform spawnPoint;
+    public int spawnNumber;
+    public float waitTime= 3f;
+    
+    private void Start(){
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn(){
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            for(int i=0; i<spawnNumber; i++) {
+                Instantiate(monster, spawnPoint.position, Quaternion.identity);
+            }
+        }
+    }
+    
+}
