d3b5adbbc426e8d6b6aea0d6542af9b8c361eaca
aluno <aluno@udicentro.local>
2019-12-02 09:42:09 -0300

    Ajustes para publicacao

---
+++ Assets/safadagem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class safadagem : MonoBehaviour
+{
+    public Animator animator;
+
+    private int LevelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        LevelToLoad = levelIndex;
+        animator.SetTrigger("Fade Out");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
